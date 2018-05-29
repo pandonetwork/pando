86cbf82672ad6ace61215742c110df25ae75d422
osarrouy <olivier@wespr.co>
2018-05-29 13:20:21 +0200

    Fix tests again

--- test/resolver.spec.js
+++ test/resolver.spec.js
@@ -6,7 +6,7 @@ const chai = require('chai')
 const dirtyChai = require('dirty-chai')
 const expect = chai.expect
 chai.use(dirtyChai)
-const CID = require('cids')
+// const CID = require('cids')
 const IpldBitcoin = require('../src/index')
 
 const snapshot = {
