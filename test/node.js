65d2eaf6d459f184e2d334ec85c81e2f20cc4a8f
osarrouy <olivier@wespr.co>
2018-06-01 11:54:31 +0200

    Lint tests

--- test/node.js
+++ test/node.js
@@ -11,16 +11,16 @@ chai.use(require('chai-as-promised'))
 
 describe('Node', () => {
   let pando, loom, index, snapshot
-  
+
   before(async () => {
     pando    = new Pando(opts)
     loom     = await pando.loom.new('test/mocks')
     index    = await loom.stage(['test/mocks/test.md', 'test/mocks/test-directory/test-1.md', 'test/mocks/test-directory/test-2.md', 'test/mocks/test-directory/test-subdirectory/test.md'])
     snapshot = await loom.snapshot('My first snapshot')
   })
-  
+
   after(async () => { await utils.fs.rmdir('test/mocks/.pando') })
-  
+
   describe('#put', async () => {
     it('should push to ipfs/ipld correctly', async () => {
       let mocks        = { test: 'data' }
@@ -30,7 +30,7 @@ describe('Node', () => {
       downloaded.should.be.deep.equal(mocks)
     })
   })
-  
+
   describe('#get', async () => {
     it('should get from ipfs/ipld correctly', async () => {
       let mocks        = { test: 'data' }
@@ -55,7 +55,7 @@ describe('Node', () => {
     })
     
   })
-  
+
   describe('#upload', async () => {
     let cid, downloaded
     
