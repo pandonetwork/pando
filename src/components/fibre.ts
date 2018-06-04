cc23f6bdb18367949622a53b3c67a5b392db7e80
Edouard Philippe <edouard.q.philippe@gmail.com>
2018-06-04 18:56:39 +0200

    Updating fibre.ts. Creating files with the fibre name containing the head of the fibre.

--- src/components/fibre.ts
+++ src/components/fibre.ts
@@ -4,7 +4,7 @@ import Specimen from '@components/specimen'
 import path_    from 'path'
 
 export default class Fibre {
-  
+
   public loom:      Loom
   public name:      string
   public specimen?: Specimen
@@ -17,34 +17,37 @@ export default class Fibre {
     this.loom = _loom
     this.name = _name
   }
-  
+
   public static async new (_loom: Loom, _name: string, opts?: any): Promise < Fibre > {
+    //Check if Fibre doesnotexist.
+    //Create a file in utils.yaml.write in fibres:   '.pando/fibres'
+    //
     return new Fibre(_loom, _name)
   }
-  
+
   // public async snapshot () : Promise < Snapshot > {
-  // 
+  //
   // }
-  
-  
+
+
   public async fetch () {
-    
+
   }
-  
+
   public async pull () {
-    
+
   }
-  
+
   public async revert () {
-    
+
   }
-  
+
   public get head (): string {
     return ''
   }
 
   public set head (_head: string) {
   }
-  
-  
+
+
 }
