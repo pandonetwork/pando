05a9e5ec634e85c2b20decc8dc88c1a513377421
Olivier Sarrouy <olivier@ryhope.network>
2018-10-10 13:44:10 +0200

    Clean truffle.js

--- packages/core/truffle.js
+++ packages/core/truffle.js
@@ -1,13 +1,9 @@
 module.exports = {
- //    compiler: {
- //     solc: "0.4.24"       // Version. ex:  "0.4.20". (Default: truffle's installed solc)
- // },
   networks: {
     rpc: {
       host: 'localhost',
       port: 8545,
       network_id: 15,
-      // gas: 6.9e6
       gas: 0xffffffffff,
       gasPrice: 0x01,
       websocket: true
