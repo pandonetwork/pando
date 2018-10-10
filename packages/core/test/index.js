92eb776987a2609823b34811a35a776b7e9d82a1
Olivier Sarrouy <olivier@ryhope.network>
2018-10-10 17:10:43 +0200

    Unlock all tests

--- packages/core/test/index.js
+++ packages/core/test/index.js
@@ -1,4 +1,4 @@
 require('./core/genesis.js')
-// require('./core/api.js')
-// require('./kits/dictator.js')
-// require('./kits/voting.js')
+require('./core/api.js')
+require('./kits/dictator.js')
+require('./kits/voting.js')
