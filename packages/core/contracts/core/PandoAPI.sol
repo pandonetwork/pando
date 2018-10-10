d9d1ce1e090aff6b67693e0c48c084be9cad4367
Olivier Sarrouy <olivier@ryhope.network>
2018-10-10 17:06:46 +0200

    Lint PandoAPI contract

--- packages/core/contracts/core/PandoAPI.sol
+++ packages/core/contracts/core/PandoAPI.sol
@@ -18,19 +18,6 @@ contract PandoAPI is AragonApp {
     bytes32 constant public ACCEPT_RFL_ROLE = keccak256("ACCEPT_RFL_ROLE");
     bytes32 constant public REJECT_RFL_ROLE = keccak256("REJECT_RFL_ROLE");
 
-    event CreateRFI(uint256 id);
-    event MergeRFI(uint256 id);
-    event RejectRFI(uint256 id);
-    event CancelRFI(uint256 id);
-
-    event CreateRFL(uint256 id);
-    event AcceptRFL(uint256 id, uint256 value);
-    event RejectRFL(uint256 id);
-    event CancelRFL(uint256 id);
-    event MintRFL(uint256 id, address destination, uint256 value); // ISSUE RFL PLUTOT
-
-    // ADD MODIFIER FOR RFIExist(_RFIid) and RFLExists(_RFLid)
-
     PandoGenesis public genesis;
     PandoLineage public lineage;
 
@@ -42,6 +29,27 @@ contract PandoAPI is AragonApp {
 
     uint256 public RFIsLength = 0;
     uint256 public RFLsLength = 0;
+
+    event CreateRFI(uint256 indexed id);
+    event MergeRFI(uint256 indexed id);
+    event RejectRFI(uint256 indexed id);
+    event CancelRFI(uint256 indexed id);
+
+    event CreateRFL(uint256 indexed id);
+    event AcceptRFL(uint256 indexed id, uint256 value);
+    event RejectRFL(uint256 indexed id);
+    event CancelRFL(uint256 indexed id);
+    event IssueRFL(uint256 indexed id, address destination, uint256 value);
+
+    modifier RFIExists(uint256 _RFIid) {
+        require(_RFIid <= RFIsLength, "RFI does not exist");
+        _;
+    }
+
+    modifier RFLExists(uint256 _RFLid) {
+        require(_RFLid <= RFLsLength, "RFL does not exist");
+        _;
+    }
 
     /*--------------------*/
 
@@ -60,60 +68,79 @@ contract PandoAPI is AragonApp {
         RFIid = _createRFI(_individuation, _lineages);
     }
 
-
     function mergeRFI(uint256 _RFIid) public isInitialized auth(MERGE_RFI_ROLE) {
-        require(_RFIid <= RFIsLength && RFIs[_RFIid].isPending(), "RFI is not pending anymore");
-
-        Pando.RFI storage RFI = RFIs[_RFIid];
-
-
-        for(uint256 i = 0; i < RFI.RFLids.length; i++) {
-            require(RFLs[RFI.RFLids[i]].isAccepted());
-        }
-
+        require(canMergeRFI(_RFIid), "cannot merge RFI");
         _mergeRFI(_RFIid);
     }
 
-    function rejectRFI(uint256 _RFIid) public isInitialized auth(REJECT_RFI_ROLE) {
-        require(_RFIid <= RFIsLength && RFIs[_RFIid].isPending());
-
+    function rejectRFI(uint256 _RFIid)public isInitialized auth(REJECT_RFI_ROLE) {
+        require(canRejectRFI(_RFIid), "cannot reject RFI");
         _rejectRFI(_RFIid);
     }
 
     function acceptRFL(uint256 _RFLid, uint256 _value) public isInitialized auth(ACCEPT_RFL_ROLE) {
-        require(_RFLid <= RFLsLength);
-
-        Pando.RFL storage RFL = RFLs[_RFLid];
-
-        require(RFL.isPending());
-        require(_value >= RFL.lineage.minimum);
-
+        require(canAcceptRFL(_RFLid, _value), "cannot accept RFL");
         _acceptRFL(_RFLid, _value);
     }
 
     function rejectRFL(uint256 _RFLid) public isInitialized auth(REJECT_RFL_ROLE) {
-        require(_RFLid <= RFLsLength);
-
-        Pando.RFL storage RFL = RFLs[_RFLid];
-
-        require(RFL.isPending());
-
+        require(canRejectRFL(_RFLid), "cannot reject RFL");
         _rejectRFL(_RFLid);
     }
 
     /*--------------------*/
 
-    function getRFI(uint256 _RFIid) public view returns (Pando.RFI) {
-        require(_RFIid <= RFIsLength);
+    function canMergeRFI(uint256 _RFIid) public view returns (bool) {
+        if (_RFIid > RFIsLength)
+            return false;
+        Pando.RFI storage RFI = RFIs[_RFIid];
+        if (RFI.state != Pando.RFIState.Pending)
+            return false;
+        for (uint256 i = 0; i < RFI.RFLids.length; i++) {
+            Pando.RFL storage RFL = RFLs[RFI.RFLids[i]];
+            if (RFL.state != Pando.RFLState.Accepted)
+                return false;
+        }
+        return true;
+    }
 
+    function canRejectRFI(uint _RFIid) public view returns (bool) {
+        if (_RFIid > RFIsLength)
+            return false;
+        if (!RFIs[_RFIid].isPending())
+            return false;
+        return true;
+    }
 
+    function canAcceptRFL(uint256 _RFLid, uint256 _value) public view returns (bool) {
+        if (_RFLid > RFLsLength)
+            return false;
+        if (!RFLs[_RFLid].isPending())
+            return false;
+        if (_value < RFLs[_RFLid].lineage.minimum)
+            return false;
+        return true;
+    }
+
+    function canRejectRFL(uint _RFLid) public view returns (bool) {
+        if (_RFLid > RFLsLength)
+            return false;
+        if (!RFLs[_RFLid].isPending())
+            return false;
+        return true;
+    }
+
+    /*--------------------*/
+
+    function getLineageToken() public view returns (address) {
+        return address(lineage.token());
+    }
+
+    function getRFI(uint256 _RFIid) public view RFIExists(_RFIid) returns (Pando.RFI) {
         return RFIs[_RFIid];
     }
 
-    function getRFL(uint256 _RFLid) public view returns (Pando.RFL) {
-        require(_RFLid <= RFLsLength);
-
-
+    function getRFL(uint256 _RFLid) public view RFLExists(_RFLid) returns (Pando.RFL) {
         return RFLs[_RFLid];
     }
 
@@ -121,7 +148,7 @@ contract PandoAPI is AragonApp {
         return genesis.head();
     }
 
-    function getHead() isInitialized public view returns (Pando.Individuation) {
+    function getHead() public view returns (Pando.Individuation) {
         return genesis.getHead();
     }
 
@@ -129,30 +156,25 @@ contract PandoAPI is AragonApp {
         return _individuation.hash();
     }
 
-    function getLineageToken() public view returns (address) {
-        return address(lineage.token());
-    }
-
     /*--------------------*/
 
-    function _createRFI(Pando.IIndividuation _individuation, Pando.ILineage[] _lineages) internal returns (uint256 RFIid)  {
-        // msg.sender is gonna be the address of a kit contract built on top of the API
-        // Do you we wanna to make sure that the transaction is actually initialized by _individuation.origin ?
+    function _createRFI(Pando.IIndividuation _individuation, Pando.ILineage[] _lineages) internal returns (uint256 RFIid) {
+        // msg.sender is gonna be the address of a governance kit built on top of the API
+        // Do you we wanna make sure that the transaction is actually initialized by _individuation.origin ?
         // If this is the case then we're gonna need to ask for a proof of identity through a ECDSA signature
         // See https://github.com/OpenZeppelin/openzeppelin-solidity/blob/master/contracts/cryptography/ECDSA.sol
 
         RFIsLength = RFIsLength + 1;
-        RFIid      = RFIsLength;
+        RFIid = RFIsLength;
 
         Pando.RFI storage RFI = RFIs[RFIid];
 
+        RFI.individuation.origin = _individuation.origin;
+        RFI.individuation.tree = _individuation.tree;
+        RFI.individuation.message = _individuation.message;
+        RFI.individuation.metadata = _individuation.metadata;
         RFI.blockstamp = block.number;
-        RFI.state      = Pando.RFIState.Pending;
-
-        RFI.individuation.origin     = _individuation.origin;
-        RFI.individuation.tree       = _individuation.tree;
-        RFI.individuation.message    = _individuation.message;
-        RFI.individuation.metadata   = _individuation.metadata;
+        RFI.state = Pando.RFIState.Pending;
 
         // solc 4.0.24 cannot directly convert dynamic struct array from memory to storage
         RFI.individuation.parents.length = _individuation.parents.length;
@@ -160,7 +182,7 @@ contract PandoAPI is AragonApp {
             RFI.individuation.parents[i] = _individuation.parents[i];
         }
 
-        for(uint256 j = 0; j < _lineages.length; j++) {
+        for (uint256 j = 0; j < _lineages.length; j++) {
             uint256 RFLid = _createRFL(_lineages[j], RFIid);
             uint256 index = RFI.RFLids.length++;
             RFI.RFLids[index] = RFLid;
@@ -211,20 +233,18 @@ contract PandoAPI is AragonApp {
     }
 
     function _createRFL(Pando.ILineage _lineage, uint256 _RFIid) internal returns (uint256 RFLid) {
-        require(RFIs[_RFIid].individuation.origin != address(0));
+        RFLsLength = RFLsLength + 1;
+        RFLid = RFLsLength;
 
-        RFLsLength            = RFLsLength + 1;
-        RFLid                 = RFLsLength;
         Pando.RFL storage RFL = RFLs[RFLid];
 
         RFL.lineage.destination = _lineage.destination;
-        RFL.lineage.minimum     = _lineage.minimum;
-        RFL.lineage.metadata    = _lineage.metadata;
-
+        RFL.lineage.minimum = _lineage.minimum;
+        RFL.lineage.metadata = _lineage.metadata;
         RFL.blockstamp = block.number;
-        RFL.value     = 0;
-        RFL.state      = Pando.RFLState.Pending;
-        RFL.RFIid      = _RFIid;
+        RFL.value = 0;
+        RFL.state = Pando.RFLState.Pending;
+        RFL.RFIid = _RFIid;
 
         emit CreateRFL(RFLid);
     }
@@ -232,7 +252,7 @@ contract PandoAPI is AragonApp {
     function _acceptRFL(uint256 _RFLid, uint256 _value) internal {
         Pando.RFL storage RFL = RFLs[_RFLid];
 
-        RFL.state  = Pando.RFLState.Accepted;
+        RFL.state = Pando.RFLState.Accepted;
         RFL.value = _value;
 
         emit AcceptRFL(_RFLid, _value);
@@ -242,6 +262,7 @@ contract PandoAPI is AragonApp {
         Pando.RFL storage RFL = RFLs[_RFLid];
 
         RFL.state = Pando.RFLState.Rejected;
+
         _cancelRFI(RFL.RFIid);
 
         emit RejectRFL(_RFLid);
@@ -259,8 +280,9 @@ contract PandoAPI is AragonApp {
         Pando.RFL storage RFL = RFLs[_RFLid];
 
         RFL.state = Pando.RFLState.Issued;
+
         lineage.mint(RFL.lineage.destination, RFL.value);
 
-        emit MintRFL(_RFLid, RFL.lineage.destination, RFL.value);
+        emit IssueRFL(_RFLid, RFL.lineage.destination, RFL.value);
     }
 }
