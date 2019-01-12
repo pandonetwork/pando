8e1bc428fdf95f90e9a03a4fa5f5f60a53ef9304
Olivier Sarrouy <olivier@pando.network>
2019-01-12 19:13:27 +0100

    Start cleaning @pando/colony package

--- packages/colony/contracts/import/Import.sol
+++ packages/colony/contracts/import/Import.sol
@@ -1,16 +1,15 @@
 pragma solidity ^0.4.24;
 pragma experimental ABIEncoderV2;
 
-import "@aragon/os/contracts/acl/ACL.sol";
 import "@aragon/os/contracts/kernel/Kernel.sol";
+import "@aragon/os/contracts/acl/ACL.sol";
+
 import "@aragon/os/contracts/factory/DAOFactory.sol";
 import "@aragon/os/contracts/factory/EVMScriptRegistryFactory.sol";
 import "@aragon/apps-token-manager/contracts/TokenManager.sol";
+import "@aragon/apps-vault/contracts/Vault.sol";
 import "@pando/organism/contracts/Organism.sol";
 import "@pando/scheme-democracy/contracts/DemocracyScheme.sol";
-import "@aragon/apps-vault/contracts/Vault.sol";
-
-
 
 
 contract Import {
