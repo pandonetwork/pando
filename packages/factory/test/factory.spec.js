58e1ff07199541ec47b4a61b0f416368969a0fbc
Olivier Sarrouy <olivier@pando.network>
2019-01-14 11:25:12 +0100

    Add ESLint / prettier

--- packages/factory/test/factory.spec.js
+++ packages/factory/test/factory.spec.js
@@ -1,23 +1,23 @@
-const Kernel              = artifacts.require('@aragon/os/contracts/kernel/Kernel.sol')
-const ACL                 = artifacts.require('@aragon/core/contracts/acl/ACL')
-const MiniMeToken         = artifacts.require("@aragon/apps-shared-minime/contracts/MiniMeToken");
-const Vault               = artifacts.require("@aragon/apps-vault/contracts/Vault")
-const Finance             = artifacts.require("@aragon/apps-finance/contracts/Finance")
-const Voting              = artifacts.require("@aragon/apps-voting/contracts/Voting")
-const TokenManager        = artifacts.require("@aragon/apps-token-manager/contracts/TokenManager")
-const Scheme              = artifacts.require("@pando/scheme-democracy/contracts/DemocracyScheme")
-const Colony              = artifacts.require("@pando/core/contracts/colony/IColony")
+/* eslint-disable no-undef */
+const Kernel = artifacts.require('@aragon/os/contracts/kernel/Kernel.sol')
+const ACL = artifacts.require('@aragon/core/contracts/acl/ACL')
+const Vault = artifacts.require('@aragon/apps-vault/contracts/Vault')
+const Finance = artifacts.require('@aragon/apps-finance/contracts/Finance')
+const Voting = artifacts.require('@aragon/apps-voting/contracts/Voting')
+const TokenManager = artifacts.require('@aragon/apps-token-manager/contracts/TokenManager')
+const Scheme = artifacts.require('@pando/scheme-democracy/contracts/DemocracyScheme')
+const Colony = artifacts.require('@pando/core/contracts/colony/IColony')
 const OrganizationFactory = artifacts.require('OrganizationFactory')
 
-const namehash                = require('eth-ens-namehash').hash
+const namehash = require('eth-ens-namehash').hash
 const { ADDR_NULL, ADDR_ANY } = require('@pando/helpers/address')
-
-const chai   = require('chai')
+const chai = require('chai')
+/* eslint-disable no-unused-vars */
 const should = chai.should()
+/* eslint-enable no-unused-vars */
 
-const arapp       = require('../arapp.json')
+const arapp = require('../arapp.json')
 const ENS_ADDRESS = arapp.environments.default.registry
-
 
 contract('OrganizationFactory', accounts => {
   let factory
@@ -37,16 +37,20 @@ contract('OrganizationFactory', accounts => {
   })
 
   context('#newInstance', () => {
-    it('it should deploy DAO', async() => {
+    it('it should deploy DAO', async () => {
       const receipt = await factory.newInstance()
 
-      address = receipt.logs.filter(l => l.event == 'DeployInstance')[0].args.dao
-      apps    = receipt.logs.filter(l => l.event == 'InstalledApp').map(event => { return { id: event.args.appId, proxy: event.args.appProxy } })
+      address = receipt.logs.filter(l => l.event === 'DeployInstance')[0].args.dao
+      apps = receipt.logs
+        .filter(l => l.event === 'InstalledApp')
+        .map(event => {
+          return { id: event.args.appId, proxy: event.args.appProxy }
+        })
 
       address.should.not.equal(ADDR_NULL)
     })
 
-    it('it should install apps', async() => {
+    it('it should install apps', async () => {
       apps[0].id.should.equal(namehash('vault.aragonpm.eth'))
       apps[1].id.should.equal(namehash('finance.aragonpm.eth'))
       apps[2].id.should.equal(namehash('token-manager.aragonpm.eth'))
@@ -54,28 +58,28 @@ contract('OrganizationFactory', accounts => {
       apps[4].id.should.equal(namehash('scheme-democracy.aragonpm.eth'))
       apps[5].id.should.equal(namehash('colony.aragonpm.eth'))
 
-      kernel       = await Kernel.at(address)
-      acl          = await ACL.at(await kernel.acl())
-      vault        = await Vault.at(apps[0].proxy)
-      finance      = await Finance.at(apps[1].proxy)
+      kernel = await Kernel.at(address)
+      acl = await ACL.at(await kernel.acl())
+      vault = await Vault.at(apps[0].proxy)
+      finance = await Finance.at(apps[1].proxy)
       tokenManager = await TokenManager.at(apps[2].proxy)
-      voting       = await Voting.at(apps[3].proxy)
-      scheme       = await Scheme.at(apps[4].proxy)
-      colony       = await Colony.at(apps[5].proxy)
+      voting = await Voting.at(apps[3].proxy)
+      scheme = await Scheme.at(apps[4].proxy)
+      colony = await Colony.at(apps[5].proxy)
     })
 
-    it('it should initialize apps', async() => {
-      (await Promise.all([
+    it('it should initialize apps', async () => {
+      ;(await Promise.all([
         vault.hasInitialized(),
         finance.hasInitialized(),
         tokenManager.hasInitialized(),
         voting.hasInitialized(),
-        scheme.hasInitialized()
+        scheme.hasInitialized(),
       ])).should.deep.equal([true, true, true, true, true])
     })
 
-    it('it should set permissions', async() => {
-      (await Promise.all([
+    it('it should set permissions', async () => {
+      ;(await Promise.all([
         kernel.hasPermission(colony.address, kernel.address, await kernel.APP_MANAGER_ROLE(), '0x0'),
         kernel.hasPermission(colony.address, acl.address, await acl.CREATE_PERMISSIONS_ROLE(), '0x0'),
         kernel.hasPermission(finance.address, vault.address, await vault.TRANSFER_ROLE(), '0x0'),
@@ -88,8 +92,8 @@ contract('OrganizationFactory', accounts => {
         kernel.hasPermission(voting.address, tokenManager.address, await tokenManager.REVOKE_VESTINGS_ROLE(), '0x0'),
         kernel.hasPermission(voting.address, tokenManager.address, await tokenManager.BURN_ROLE(), '0x0'),
         kernel.hasPermission(ADDR_ANY, voting.address, await voting.CREATE_VOTES_ROLE(), '0x0'),
-        kernel.hasPermission(voting.address, scheme.address, await  scheme.UPDATE_PARAMETERS_ROLE(), '0x0'),
-        kernel.hasPermission(ADDR_ANY, colony.address, await colony.DEPLOY_ORGANISM_ROLE(), '0x0')
+        kernel.hasPermission(voting.address, scheme.address, await scheme.UPDATE_PARAMETERS_ROLE(), '0x0'),
+        kernel.hasPermission(ADDR_ANY, colony.address, await colony.DEPLOY_ORGANISM_ROLE(), '0x0'),
       ])).should.deep.equal([true, true, true, true, true, true, true, true, true, true, true, true, true, true])
     })
   })
