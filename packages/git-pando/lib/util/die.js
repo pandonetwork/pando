0aa97befb03279f53471cf053ac2c0337fe3512c
Olivier Sarrouy <olivier@pando.network>
2019-02-24 14:16:39 +0100

    Move build directories from lib/ to build/

--- packages/git-pando/lib/util/die.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (function (message) {
-    console.error(message);
-    process.exit(1);
-});
-//# sourceMappingURL=die.js.map
