bd8cb11e742d85b4c948d4d00ded6a7524770099
Olivier Sarrouy <olivier@pando.network>
2019-03-22 15:40:47 +0100

    Add commit history filtering and ordering

--- packages/pando-repository/app/src/script.js
+++ packages/pando-repository/app/src/script.js
@@ -4,6 +4,9 @@ import IPFS from 'ipfs-http-client'
 import IPLD from 'ipld'
 import IPLDGit from 'ipld-git'
 import { parsePersonLine, cidToSha } from 'ipld-git/src/util/util.js'
+import uniqWith from 'lodash.uniqwith'
+import orderBy from 'lodash.orderby'
+
 
 const app = new Aragon()
 const ipfs = IPFS({ host: 'localhost', port: '5001', protocol: 'http' })
@@ -32,8 +35,22 @@ app.store(async (state, event) => {
         // state.branches[branchFromRef(event.returnValues.ref)] =
         //   event.returnValues.hash
         console.log('Fetching history')
-        state.branches[branchFromRef(event.returnValues.ref)] = await fetchHistory(event.returnValues.hash, [])
+        let history = await fetchHistory(event.returnValues.hash, [])
+        history = uniqWith(history, (one, two) => {
+          return one.cid === two.cid
+        })
+        history = orderBy(history, [(commit) => {
+          // https://github.com/git/git/blob/v2.3.0/Documentation/date-formats.txt
+          const [timestamp, offset] = commit.author.date.split(' ')
+          // need to handle offset too
+          return timestamp
+          // console.log(timestamp*1000)
+          // console.log((new Date(timestamp)).toString())
+          // return new Date(timestamp)
+        }], ['desc'])
+        state.branches[branchFromRef(event.returnValues.ref)] = history
         console.log('History fetched')
+        console.log(history)
       } catch (err) {
         console.error('Failed to load commit history due to:', err)
       }
