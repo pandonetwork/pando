8af4894a6fcc2d9e5d031d4021e42289fb7ba0cf
Olivier Sarrouy <olivier@pando.network>
2019-03-26 19:40:24 +0100

    Add rinkeby network

--- truffle.js
+++ truffle.js
@@ -1,11 +1,18 @@
 module.exports = {
   networks: {
-    devnet: {
-      host: "localhost",
-      port: 8545,
+    devchain: {
+      host: "localhost", // Aragon devchain
+      port: 8545, // Aragon devchain
       network_id: "*",
       gas: 50e6,
       gasPrice: 1500000000
+    },
+    rinkeby: {
+      host: "localhost", // Frame
+      port: "1248", // Frame
+      network_id: 4,
+      gas: 7587240,
+      gasPrice: 15000000001
     }
   },
   compilers: {
