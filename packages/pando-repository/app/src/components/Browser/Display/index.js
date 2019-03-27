918700da7a69caa7c5d81c38b8bf585d55a75123
deam <deamme@users.noreply.github.com>
2019-03-27 13:37:15 +0100

    PR tab, diffing and code highlight update

--- packages/pando-repository/app/src/components/Browser/Display/index.js
+++ packages/pando-repository/app/src/components/Browser/Display/index.js
@@ -1,13 +1,43 @@
-import Highlight, { defaultProps } from 'prism-react-renderer'
+import Prism from 'prismjs'
+import 'prismjs/components/prism-actionscript'
+import 'prismjs/components/prism-bash'
+import 'prismjs/components/prism-c'
+import 'prismjs/components/prism-coffeescript'
+import 'prismjs/components/prism-cpp'
+import 'prismjs/components/prism-css-extras'
+import 'prismjs/components/prism-diff'
+import 'prismjs/components/prism-docker'
+import 'prismjs/components/prism-elixir'
+import 'prismjs/components/prism-erlang'
+import 'prismjs/components/prism-git'
+import 'prismjs/components/prism-go'
+import 'prismjs/components/prism-graphql'
+import 'prismjs/components/prism-handlebars'
+import 'prismjs/components/prism-haskell'
+import 'prismjs/components/prism-java'
+import 'prismjs/components/prism-jsx'
+import 'prismjs/components/prism-latex'
+import 'prismjs/components/prism-makefile'
+import 'prismjs/components/prism-markdown'
+import 'prismjs/components/prism-markup-templating'
+import 'prismjs/components/prism-objectivec'
+import 'prismjs/components/prism-ocaml'
+import 'prismjs/components/prism-php'
+import 'prismjs/components/prism-python'
+import 'prismjs/components/prism-reason'
+import 'prismjs/components/prism-ruby'
+import 'prismjs/components/prism-rust'
+import 'prismjs/components/prism-sass'
+import 'prismjs/components/prism-scss'
+import 'prismjs/components/prism-sql'
+import 'prismjs/components/prism-swift'
+import 'prismjs/components/prism-typescript'
+import 'prismjs/components/prism-vim'
+import 'prismjs/components/prism-yaml'
 import React from 'react'
 import ReactMarkdown from 'react-markdown/with-html'
-import styled, { createGlobalStyle } from 'styled-components'
+import styled from 'styled-components'
 import { prismMapping } from '../constants'
-import Theme from '../Theme'
-
-const MonoStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css?family=Overpass+Mono:300');
-`
 
 const MarkdownWrapper = styled.div`
   padding: 2rem;
@@ -78,32 +108,33 @@ const MarkdownWrapper = styled.div`
 
 export default class Display extends React.Component {
   render() {
-    const { file, filename } = this.props
-    const normalizedFile = file.split('\u0000')[1] // TODO: find a better way to get rid of "blob 2610\u0000"
+    const { file, filename, removeBorder, plain } = this.props
+
+    const splittedFile = file.split('\u0000')
+    let normalizedFile = file
+
+    if (splittedFile.length > 1) {
+      normalizedFile = file.split('\u0000')[1] // TODO: find a better way to get rid of "blob 2610\u0000"
+    }
+
     const language = prismMapping[filename.split('.').pop()]
 
     return (
-      <Wrapper>
-        <MonoStyle />
-        {language === 'markdown' && (
+      <Wrapper removeBorder={removeBorder}>
+        {!plain && language === 'markdown' && (
           <MarkdownWrapper>
             <ReactMarkdown source={normalizedFile} escapeHtml={false} />
           </MarkdownWrapper>
         )}
-        {language !== 'markdown' && (
-          <Highlight {...defaultProps} code={normalizedFile} language={language} theme={Theme}>
-            {({ className, style, tokens, getLineProps, getTokenProps }) => (
-              <pre className={className} style={style}>
-                {tokens.map((line, i) => (
-                  <div {...getLineProps({ line, key: i })}>
-                    {line.map((token, key) => (
-                      <span {...getTokenProps({ token, key })} />
-                    ))}
-                  </div>
-                ))}
-              </pre>
-            )}
-          </Highlight>
+        {(plain || language !== 'markdown') && (
+          <pre
+            dangerouslySetInnerHTML={{
+              __html: Prism.highlight(
+                normalizedFile,
+                Prism.languages[language]
+              ),
+            }}
+          />
         )}
       </Wrapper>
     )
@@ -111,9 +142,11 @@ export default class Display extends React.Component {
 }
 
 const Wrapper = styled.div`
+  @import url('https://fonts.googleapis.com/css?family=Overpass+Mono:300');
+
   margin-top: 0.5rem;
   padding: 10px;
-  border: 1px solid #e6e6e6;
+  border: ${({ removeBorder }) => (removeBorder ? '' : '1px solid #e6e6e6')};
   border-radius: 3px;
   background-color: white;
   overflow-x: scroll;
@@ -121,5 +154,35 @@ const Wrapper = styled.div`
   pre {
     font-family: 'Overpass Mono';
     font-size: 0.9em;
+    margin: 0;
+  }
+  .punctuation {
+    color: #24292e;
+  }
+  .keyword,
+  .url,
+  .number {
+    color: #d73a49;
+  }
+  .string,
+  .attr-value {
+    color: #032f62;
+  }
+  .function,
+  .attr-name,
+  .property {
+    color: #6f42c1;
+  }
+  .function-variable {
+    color: #005cc5;
+  }
+  .comment {
+    color: #6a737d;
+  }
+  .doctype,
+  .tag,
+  .title,
+  .selector {
+    color: #22863a;
   }
 `
