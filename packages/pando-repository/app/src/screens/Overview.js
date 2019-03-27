ddfeb4d8f6d97d0d7d0a4dfe8876d0bfd624b748
Olivier Sarrouy <olivier@pando.network>
2019-03-27 10:06:51 +0100

    Update and clean overview tab

--- packages/pando-repository/app/src/screens/Overview.js
+++ packages/pando-repository/app/src/screens/Overview.js
@@ -1,4 +1,4 @@
-import { EmptyStateCard, Table, TableHeader, TableRow, TableCell, Text, theme } from '@aragon/ui'
+import { EmptyStateCard, Table, TableRow, TableCell, Text, theme } from '@aragon/ui'
 import CID from 'cids'
 import IPFS from 'ipfs-http-client'
 import IPLD from 'ipld'
@@ -8,11 +8,23 @@ import styled from 'styled-components'
 import Box from '../components/Box'
 import Display from '../components/Browser/Display'
 
+const EMPTY_REPO = `\u0000
+  ## Your repository is empty
+
+  Make a commit and \`git push\` to this pando repo!
+`
+
+const NO_README = `\u0000
+  ## No README.md file found
+
+  We couldn't find a README.md file in your master branch. Considering adding one to tell everyone what this repo is about!
+`
+
 export default class Overview extends React.Component {
   static defaultProps = {
     branches: [],
     name: 'Loading...',
-    description: 'Loading...'
+    description: 'Loading...',
   }
 
   constructor(props) {
@@ -29,35 +41,10 @@ export default class Overview extends React.Component {
 
   componentDidMount() {
     this.deriveReadmeFromBranches(this.props.branches, 0, 0)
-
-
-    // if (branches.length) {
-    //   this.get(branches[Object.keys(branches)[0]][0], 'tree').then(tree => {
-    //     if (tree['README.md']) {
-    //       this.get(tree['README.md'].hash['/']).then(buffer => {
-    //         const content = buffer.toString()
-    //         this.setState({ readme: content })
-    //       })
-    //     }
-    //   })
-    // }
   }
 
   componentWillReceiveProps(props) {
-    console.log('RECEVING PROPS')
-    console.log(props)
-
     this.deriveReadmeFromBranches(props.branches, 0, 0)
-    // if (Object.keys(props.branches).length && props.branches[Object.keys(props.branches)[this.state.activeBranch]][0]) {
-    //   const commit = props.branches[Object.keys(props.branches)[this.state.activeBranch]][0]
-    //   console.log('commit')
-    //   console.log(commit)
-    //   this.get(commit.cid, 'tree').then(tree => {
-    //     this.setState({ commit, tree, nav: [props.name] })
-    //     console.log('STATE FROM PROPS FROM CONSTRUCTOR')
-    //     console.log(state)
-    //   })
-    // }
   }
 
   deriveCommitFromBranch(branches, branchId, commitId) {
@@ -69,23 +56,19 @@ export default class Overview extends React.Component {
   }
 
   deriveReadmeFromBranches(branches) {
-    console.log('DERIVE README FROM BRANCHES')
     const commit = this.deriveCommitFromBranch(branches, 0, 0)
 
-    console.log('COMMIT')
-    console.log(commit)
     if (commit) {
       this.get(commit.cid, 'tree').then(tree => {
         if (tree['README.md']) {
-          console.log('DO HAVE A README')
-          this.get(commit.cid, 'tree/README.md/hash').then(buffer => {
-            const content = buffer.toString()
-            console.log('GOT README')
-            console.log(content)
-            this.setState({ readme: content })
-          })
-        } else {
-          console.log('WE DONT HAVE A README')
+          this.get(commit.cid, 'tree/README.md/hash')
+            .then(buffer => {
+              const content = buffer.toString()
+              this.setState({ readme: content })
+            })
+            .catch(err => {
+              console.error('Failed to load README.md due to: ' + err)
+            })
         }
       })
     }
@@ -94,6 +77,7 @@ export default class Overview extends React.Component {
   async get(hash, path) {
     return new Promise((resolve, reject) => {
       const cid = CID.isCID(hash) ? hash : new CID(hash)
+
       this.ipld.get(cid, path, (err, result) => {
         if (err) {
           reject(err)
@@ -111,47 +95,45 @@ export default class Overview extends React.Component {
     const nbOfBranches = Object.keys(branches).length
     const nbOfCommits = branches[Object.keys(branches)[0]] ? branches[Object.keys(branches)[0]].length : 0
 
-    if (branches.length === 0) {
-      return (
-        <Box
-          display="flex"
-          height="80vh"
-          justifyContent="center"
-          alignItems="center"
-        >
-          <EmptyStateCard
-            title="Repository is empty"
-            text="Make an initial commit to the repository to get started."
-          />
-        </Box>
-      )
-    }
-
     return (
       <div>
         <InsideWrapper>
-          <Text size="xlarge" weight="bold">{name}</Text><br/>
+          <Text size="xlarge" weight="bold">
+            {name}
+          </Text>
+          <br />
           <Text>{description}</Text>
         </InsideWrapper>
 
-        <InsideWrapper>
+        {Object.keys(branches).length === 0 && <Display file={EMPTY_REPO} filename="README.md" />}
 
-          <Table>
-            <TableRow>
-              <TableCellCentered>
-                <Text><Text weight="bold">{nbOfBranches}</Text> branches</Text>
-              </TableCellCentered>
-              <TableCellCentered>
-                <Text><Text weight="bold">{nbOfCommits}</Text> commits</Text>
-              </TableCellCentered>
-              <TableCellCentered>
-                <Text><Text weight="bold">X</Text> contributors <Text color={theme.textTertiary}> [coming soon]</Text></Text>
-              </TableCellCentered>
-            </TableRow>
-          </Table>
-        </InsideWrapper>
+        {Object.keys(branches).length > 0 && (
+          <InsideWrapper>
+            <Table>
+              <TableRow>
+                <TableCellCentered>
+                  <Text>
+                    <Text weight="bold">{nbOfBranches}</Text> branches
+                  </Text>
+                </TableCellCentered>
+                <TableCellCentered>
+                  <Text>
+                    <Text weight="bold">{nbOfCommits}</Text> commits
+                  </Text>
+                </TableCellCentered>
+                <TableCellCentered>
+                  <Text>
+                    <Text weight="bold">X</Text> contributors <Text color={theme.textTertiary}> [coming soon]</Text>
+                  </Text>
+                </TableCellCentered>
+              </TableRow>
+            </Table>
+          </InsideWrapper>
+        )}
 
-        {readme && <Display file={readme} filename="README.md" />}
+        {Object.keys(branches).length > 0 && readme && <Display file={readme} filename="README.md" />}
+
+        {Object.keys(branches).length > 0 && !readme && <Display file={NO_README} filename="README.md" />}
       </div>
     )
   }
