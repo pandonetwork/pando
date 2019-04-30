c418140727971279a2f6e32fbcab948a438d7196
cd10012 <ced361@nyu.edu>
2019-04-30 15:46:41 +0200

    refactor

--- packages/pando-repository/app/src/lib/pando.ts
+++ packages/pando-repository/app/src/lib/pando.ts
@@ -1,8 +1,13 @@
 import { IPLD } from 'ipld'
+import IPFS from 'ipfs-http-client'
 import IPLDGit from 'ipld-git'
+import CID from 'cids'
 import { cidToSha } from 'ipld-git/src/util/util.js'
-//const ipfs = IPFS({ host: 'ipfs.infura.io', port: '5001', protocol: 'https' })
-//const ipld = new IPLD({ blockService: ipfs.block, formats: [IPLDGit] })
+import orderBy from 'lodash.orderby'
+import uniqWith from 'lodash.uniqwith'
+
+const ipfs = IPFS({ host: 'ipfs.infura.io', port: '5001', protocol: 'https' })
+const ipld = new IPLD({ blockService: ipfs.block, formats: [IPLDGit] })
 
 export interface IAuthorOrCommitter {
   name: string
@@ -13,48 +18,154 @@ export interface IAuthorOrCommitter {
 export interface ITree { [path: string]: string }
 
 export interface ILinkedDataCommit {
-  author: AuthorOrCommiter
-  committer: AuthorOrCommiter
+  author: IAuthorOrCommitter
+  committer: IAuthorOrCommitter
   message: string
-  parents: (ITree | string)[] | null
-  tree: Tree
+  parents: ITree[] | null
+  tree: ITree
 }
 
-export default class Commit {
-  constructor(commit: ILinkedDataCommit) {
-    if (commit.tree.hasOwnProperty('/')) {
-      Object.assign(this, commit, {
-        cid: commit.tree['/'],
-        sha: cidToSha(commit.tree['/'])
-      });
-    }
+export interface IModifiedTree {
+  [path: string]: { 
+    lastEdit: { 
+      date: string 
+      message: string
+    } 
+    mode: string | number
+    blob:  string
   }
-
-  //public static async fetchOrderedHistory(formerHistory: (Commit | null)[]) {}
 }
 
-//public static async _fetchModifiedTree(commit: ILinkedDataCommit): Promise<ModifiedC
+export const fetchFilesFromTree = (hash: string, path?: string) => {
+  return new Promise(async (resolve, reject) => {
+    let files = {}
+    const cid = new CID(hash)
 
-/** async function fetchModifiedTree(root: Tree): Promise<ModifiedTree> {
-  //{ "/" : cid, "/": cid } ---> { "/": lastModified: { date: "datestr", author: "string"}, mode: "int", blob: cid }
-  /**return new Promise<ModifiedTree>((resolve, reject) => {
+    ipld.get(cid, async (err, result) => {
+      if (err) {
+        reject(err)
+      } else {
+        const tree = result.value
+        for (let entry in tree) {
+          const _path = path ? path + '/' + entry : entry
+          if (tree[entry].mode === '40000') {
+            const _files = await fetchFilesFromTree(tree[entry]['hash']['/'], _path)
+            files = { ...files, ..._files }
+          } else {
+            files[_path].lastEdit = {
+              date: tree[entry]['committer'].date,
+              message: tree[entry]['message']
+            }
+            files[_path].mode = tree[entry].mode
+            files[_path].blob = new CID(tree[entry]['hash']['/']).toBaseEncodedString()
+          }
+        }
+        resolve(files as IModifiedTree)
+      }
+    })
+  })
+}
+
+export const fetchHistory = async (hash:string, history: Commit[]): Promise<Commit[]> => {
+  return new Promise(async (resolve, reject) => {
     try {
-      const cid = root["/"].cid
+      const commit = await fetchCommit(hash)
+      history.push(commit)
 
+      for (let parent of commit.parents) {
+        history = await fetchHistory(parent['/'], history)
+      }
+
+      resolve(history)
+    } catch (err) {
+      reject(err)
+    }
+  })
+}
+
+export const fetchCommit = async (hash: string): Promise<Commit> => {
+  return new Promise((resolve, reject) => {
+    try {
+      const cid = new CID(hash)
       ipld.get(cid, async (err, result) => {
         if (err) {
           reject(err)
         } else {
-
+          const IPLDCommit = result.value as ILinkedDataCommit
+          const commit = new Commit(IPLDCommit)
+          await commit.fetchModifiedTree()
+          resolve(commit)
         }
       })
     } catch (err) {
-      reject(err);
+      reject(err)
     }
   })
-} **/
+}
 
-//export default fetchModifiedTree;
-//export function convertCommitToIPLDCommit(commit: Commit): IPLDCommit {}
+export const fetchOrderedHistory = async (hash: string, formerHistory: Commit[]) => {
+  let history = await fetchHistory(hash, formerHistory)
+  history = uniqWith(history, (one, two) => {
+    return one.cid === two.cid
+  })
+
+  history = orderBy(
+    history,
+    [
+      commit => {
+        // https://github.com/git/git/blob/v2.3.0/Documentation/date-formats.txt
+        /* eslint-disable-next-line no-unused-vars */
+        const [timestamp, offset] = commit.author.date.split(' ')
+        return timestamp
+      },
+    ],
+    ['desc']
+  )
+
+  return history
+}
+
+export default class Commit {
+  cid: string
+  sha: string
+  author: IAuthorOrCommitter
+  committer: IAuthorOrCommitter
+  message: string
+  parents: ITree[] | string[]
+  tree: ITree | IModifiedTree | {}
+
+  constructor(commit: ILinkedDataCommit) {
+    if (commit.tree.hasOwnProperty('/')) {
+      const _cid = new CID(commit.tree['/'])
+      let _parents : string[] = commit.parents ? 
+        commit.parents.map(parent => (new CID(Object.values(parent)[0]).toBaseEncodedString)) : [];
+        this.cid = _cid
+        this.sha = cidToSha(commit.tree['/']).toString('hex')
+        this.author = commit.author
+        this.committer = commit.committer
+        this.message = commit.message
+        this.tree = commit.tree
+        this.parents = _parents
+    }
+  }
+
+  public async fetchModifiedTree(): Promise<void> {
+    const files = await fetchFilesFromTree(this.tree['/'])
+    this.tree = files
+  }
+
+  public async getOrderdHistory(): Promise<Commit[]> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const orderedHistory = await fetchOrderedHistory(this.cid, [])
+        resolve(orderedHistory)
+      } catch (err) {
+        reject(err)
+      }
+    })
+  }
+}
+
+//export function convertCommitToIPLDCommit(commit: Commit): ILinkedDataCommit {}
 
 
